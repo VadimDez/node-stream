a65f72cab15d45c52a3f00dcf9a8af808e6ba2eb
Vadym Yatsyuk <vasp3d@gmail.com>
2015-10-10 14:04:11 +0200

    read stream

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-    fs.readFile(__dirname + '/data.txt', function (err, data) {
-        res.end(data);
-    });
+    var stream = fs.createReadStream(__dirname + '/data.txt');
+    stream.pipe(res);
 });
 
 server.listen(8000);
