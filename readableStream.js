547716e668afc36e65873ce97932716b6bcb0fde
Vadym Yatsyuk <vasp3d@gmail.com>
2015-10-10 14:31:11 +0200

    readable

--- readableStream.js
+++ readableStream.js
@@ -4,12 +4,20 @@
 var Readable = require('stream').Readable;
 var rs = Readable();
 
-var c = 97;
+var c = 97 - 1;
 rs._read = function () {
-    rs.push(String.fromCharCode(c++));
     if (c > 'z'.charCodeAt(0)) {
-        rs.push(null);
+        return rs.push(null);
     }
+
+    setTimeout(function () {
+        rs.push(String.fromCharCode(c++));
+    }, 100);
 };
 
 rs.pipe(process.stdout);
+
+process.on('exit', function () {
+    console.error('\n_read() called ' + (c - 97) + ' times');
+});
+process.stdout.on('error', process.exit);
