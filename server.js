701334f257625643a1d3097497e14f7844eb7bbb
Vadym Yatsyuk <vasp3d@gmail.com>
2015-10-10 14:11:50 +0200

    added oppressor for compressing

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
  */
 var http = require('http');
 var fs = require('fs');
+var oppressor = require('oppressor');
 
 var server = http.createServer(function (req, res) {
     var stream = fs.createReadStream(__dirname + '/data.txt');
-    stream.pipe(res);
+    stream.pipe(oppressor(req)).pipe(res);
 });
 
 server.listen(8000);
