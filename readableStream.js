f2b42815a478f917391782d4e966d3344c984bea
Vadym Yatsyuk <vasp3d@gmail.com>
2015-10-10 14:19:11 +0200

    readable stream

--- readableStream.js
+++ readableStream.js
@@ -1,3 +1,10 @@
 /**
  * Created by Vadym on 10/10/15.
  */
+var Readable = require('stream').Readable;
+var rs = new Readable;
+rs.push('test');
+rs.push('test test \n');
+rs.push(null); // done outputting data
+
+rs.pipe(process.stdout);
